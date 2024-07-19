7714383ac5fb955a2f2e0e63ffea5698af6d6ab8
hwang <cararito@naver.com>
2024-07-20 01:18:58 +0900

    cognito 修正

--- src/main/java/com/example/cognito_demo_b/config/CognitoOidcLogoutSuccessHandler.java
+++ src/main/java/com/example/cognito_demo_b/config/CognitoOidcLogoutSuccessHandler.java
@@ -11,15 +11,15 @@ import java.io.IOException;
 @Component
 public class CognitoOidcLogoutSuccessHandler implements LogoutSuccessHandler {
 
-    private final String clientId = "plspejrcgdp29h669nkj6p96m";
-    private final String logoutUrl = "https://web01.auth.us-east-1.amazoncognito.com/logout";
-    private final String redirectUri = "http://localhost:8080/";
+    private final String clientId = "1ic6cvkuk1j3qekuephv925odg";
+    private final String logoutUrl = "https://web02.auth.us-east-1.amazoncognito.com/logout";
+    private final String redirectUriToSiteA = "http://localhost:8080/siteA";
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         String url = UriComponentsBuilder.fromHttpUrl(logoutUrl)
                 .queryParam("client_id", clientId)
-                .queryParam("logout_uri", redirectUri)
+                .queryParam("logout_uri", redirectUriToSiteA)
                 .build().toUriString();
         response.sendRedirect(url);
     }
--- src/main/java/com/example/cognito_demo_b/controller/BasicController.java
+++ src/main/java/com/example/cognito_demo_b/controller/BasicController.java
@@ -1,25 +1,17 @@
 package com.example.cognito_demo_b.controller;
 
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BasicController {
 
-    private final JwtDecoder jwtDecoder;
-
-    public BasicController(JwtDecoder jwtDecoder) {
-        this.jwtDecoder = jwtDecoder;
-    }
-
     @GetMapping("/siteB")
-    public String protectedPage(@RequestParam(name = "token") String token, Model model) {
-        Jwt jwt = jwtDecoder.decode(token);
-        model.addAttribute("username", jwt.getClaim("username"));
-        return "siteB";  // siteB.htmlに対応するテンプレート
+    public String login(Authentication authentication){
+        if (authentication != null && authentication.isAuthenticated()) {
+            return "redirect:/siteB";
+        }
+        return "siteB";
     }
 }
